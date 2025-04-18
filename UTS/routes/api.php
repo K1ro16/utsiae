6bb3c396d8648a37d5c3b5d6c3b57f0153c6e9b2
Anon9m0us <freedomkiro16@gmail.com>
2025-04-18 14:52:03 +0700

    fix route

--- UTS/routes/api.php
+++ UTS/routes/api.php
@@ -1,9 +1,17 @@
 <?php
 
-
 use App\Http\Controllers\GradeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\CourseController;
+
+//Course
+Route::get('/courses', [CourseController::class, 'index']);
+Route::get('/courses/{id}', [CourseController::class, 'show']);
+Route::post('/courses', [CourseController::class, 'store']);
+Route::put('/courses/{id}', [CourseController::class, 'update']);
+Route::post('/enrollments', [CourseController::class, 'enroll']);
+Route::get('/courses/{id}/enrollments', [CourseController::class, 'getEnrollments']);
 
 //grades
 Route::get('/grades/{id}', [GradeController::class, 'show']);
@@ -17,4 +25,3 @@ Route::get('/students/{id}', [StudentController::class, 'show']);
 Route::post('/students', [StudentController::class, 'store']);
 Route::put('/students/{id}', [StudentController::class, 'update']);
 Route::get('/students/{id}/transcript', [StudentController::class, 'getTranscript']);
-\
--- UTS/routes/web.php
+++ UTS/routes/web.php
@@ -1,14 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CourseController;
-
-Route::get('/courses', [CourseController::class, 'index']);
-Route::get('/courses/{id}', [CourseController::class, 'show']);
-Route::post('/courses', [CourseController::class, 'store']);
-Route::put('/courses/{id}', [CourseController::class, 'update']);
-Route::post('/enrollments', [CourseController::class, 'enroll']);
-Route::get('/courses/{id}/enrollments', [CourseController::class, 'getEnrollments']);
 
 Route::get('/', function () {
     return view('welcome');
